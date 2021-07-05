325f5004d4239d6aedfa63a6d91ef0f4ccea1734
MikeyJLau <mikey.lau@traininblocks.com>
2021-07-05 18:57:40 +0100

    Added legals

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,21 @@
 import Header from './Header'
+import styles from '../styles/Layout.module.css'
 
 export default function Layout ({ children }) {
         return  (
         <div>
             <Header />            
             { children }
+            <footer className={ styles.footer }>
+                <div>
+                    <p>
+                        <b>Environment First</b> Copyright 2021
+                    </p>
+                </div>
+                <div>
+                
+                </div>
+            </footer>
         </div>
     )
 }
