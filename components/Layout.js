05e4a0916fe6e4662c600a9e10dc7f9367ba2a4e
MikeyJLau <mikey.lau@traininblocks.com>
2021-07-06 22:49:55 +0100

    Added page transitions

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,33 @@
 import Header from './Header'
+import { useState, useEffect } from "react";
 import styles from '../styles/Layout.module.css'
 
 export default function Layout ({ children }) {
-        return  (
+    const [displayChildren, setDisplayChildren] = useState(children)
+    const [transitionStage, setTransitionStage] = useState('fadeOut')
+
+    useEffect(() => {
+        setTransitionStage('fadeIn');
+    }, []);
+
+    useEffect(() => {
+        if (children !== displayChildren) setTransitionStage('fadeOut');
+    }, [children, setDisplayChildren, displayChildren]);
+
+    return  (
         <div>
-            <Header />            
-            { children }
+            <Header /> 
+            <div
+                onTransitionEnd={ () => {
+                    if (transitionStage == 'fadeOut') {
+                        setDisplayChildren(children)
+                        setTransitionStage('fadeIn')
+                    }
+                } }
+                className={ `${styles.content} ${styles[transitionStage]}` }
+            >
+                { displayChildren }   
+            </div>
             <footer className={ styles.footer }>
                 <div>
                     <p>
